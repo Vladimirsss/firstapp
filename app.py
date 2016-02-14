0b3d82e10cc8fa748bc7aad45cbccff53a793308
Vladimirsss <vsazonov@cisco.com>
2016-02-14 08:30:46 +0000

    updated with Spark API

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
 
-app = Flask(__name__)
+# import requests library
+import requests
 
-@app.route("/")
-def hello():
-    return "Hello Vladimirs!!"
+#import json library
+import json
 
+# put the ip address or dns of your apic-em controller in this url
+url = 'https://api.ciscospark.com/v1/people/me'
 
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+#the username and password to access the APIC-EM Controller
+#payload = {"username":"admin","password":"C!sc0123"}
+payload = {}
+
+#Content type must be included in the header
+header = {"Authorisation": "Bearer M2RkNzJjNGQtY2UwNS00NGJkLThiOTgtNmQ2Y2VjYzdjMDI0YWY3N2ZjZGEtYzUz"}
+header = {"content-type": "application/json"}
+
+#Performs a POST on the specified url.
+response= requests.post(url,data=json.dumps(payload), headers=header, verify=False)
+
+# print the json that is returned
+print(response.text)
