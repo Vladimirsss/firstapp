f1c6951a9192c92c80c5569fc4f12c9c6430d7f2
Vladimirsss <vsazonov@cisco.com>
2016-02-14 11:57:24 +0000

    6

--- app.py
+++ app.py
@@ -2,39 +2,50 @@
 import requests
 from urllib.request import Request, urlopen
 import json
+import time
 
 
 print("<<<start>>>")
 
-# put the ip address or dns of your apic-em controller in this url
-url = 'https://api.ciscospark.com/v1/people/me'
-
-#Content type must be included in the header
+myroomid = "Y2lzY29zcGFyazovL3VzL1JPT00vYjE4YzJlMjAtY2ZjMS0xMWU1LWJmY2YtZTE4YTBkMzMwYTg1"
 header = {
     "Authorization": "Bearer M2RkNzJjNGQtY2UwNS00NGJkLThiOTgtNmQ2Y2VjYzdjMDI0YWY3N2ZjZGEtYzUz",
     "Content-type": "application/json"}
+
+#url = 'https://api.ciscospark.com/v1/people/me'
 
 #response= requests.post(url,data=json.dumps(payload), headers=header, verify=False)
-responseString= requests.get(url, headers=header)
-print("Response code: "+ str(responseString.status_code))
+#responseString= requests.get(url, headers=header)
+#print("Response code: "+ str(responseString.status_code))
 
-ro=responseString.json()
-print(ro["displayName"])
+#ro=responseString.json()
+#print(ro["displayName"])
 
-header = {
-    "Authorization": "Bearer M2RkNzJjNGQtY2UwNS00NGJkLThiOTgtNmQ2Y2VjYzdjMDI0YWY3N2ZjZGEtYzUz",
-    "Content-type": "application/json"}
+#imageURL = "http://hamble.net/WP/wp-content/uploads/2012/05/airhamble2.jpg"
+#localtime = time.asctime( time.localtime(time.time()) )
+#messsageText = str(localtime) + "> New Message"
+#payload = {
+#    "roomId":myroomid,
+#    "text": messsageText,
+#    "files":[imageURL]
+#}
 
-payload = {
-    "roomId":"Y2lzY29zcGFyazovL3VzL1JPT00vYjE4YzJlMjAtY2ZjMS0xMWU1LWJmY2YtZTE4YTBkMzMwYTg1",
-    "text":"message from myApp"
-}
+#responseString=requests.post('https://api.ciscospark.com/v1/messages',data=json.dumps(payload),headers=header)
 
-responseString=requests.post('https://api.ciscospark.com/v1/messages',data=json.dumps(payload),headers=header)
+
+requestURL="https://api.ciscospark.com/v1/messages"+"?"+"roomId="+str(myroomid)+"&"+"max=1"
+
+#print(requestURL)
+responseString=requests.get(requestURL,headers=header)
 
 print("POST response:"+str(responseString.status_code))
 ro=responseString.json()
-print("Created: "+str(ro["created"]))
+
+#msgdetails = json.loads(responseString)
+print(ro)
+#print(ro["text"])
+
+#print("Message: "+ msgdetails["text"])
 
 
 #floors = jsonObject["Building"]["Floor"]
