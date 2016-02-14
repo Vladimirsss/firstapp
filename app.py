39c700637e0a9e42b16aacea55577894a46e1da4
Vladimirsss <vsazonov@cisco.com>
2016-02-14 09:08:09 +0000

    3

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 
-# import requests library
 import requests
-
-#import json library
+from urllib.request import Request, urlopen
 import json
+
 
 print("this is first app")
 # put the ip address or dns of your apic-em controller in this url
@@ -14,11 +13,40 @@ url = 'https://api.ciscospark.com/v1/people/me'
 payload = {}
 
 #Content type must be included in the header
-header = {"Authorisation": "Bearer M2RkNzJjNGQtY2UwNS00NGJkLThiOTgtNmQ2Y2VjYzdjMDI0YWY3N2ZjZGEtYzUz"}
-header = {"content-type": "application/json"}
+header = {
+    "Authorization": "Bearer M2RkNzJjNGQtY2UwNS00NGJkLThiOTgtNmQ2Y2VjYzdjMDI0YWY3N2ZjZGEtYzUz",
+    "Content-type": "application/json"}
 
+print (header)
 #Performs a POST on the specified url.
-response= requests.post(url,data=json.dumps(payload), headers=header, verify=False)
+#response= requests.get(url,data=json.dumps(payload), headers=header, verify=False)
+responseString= requests.get(url, headers=header)
 
 # print the json that is returned
-print("Response: " + response.text)
+print("Response code: "+ str(responseString.status_code))
+
+print(responseString.json())
+
+
+#req = Request('https://msesandbox.cisco.com/api/contextaware/v1/maps/info/DevNetCampus/DevNetBuilding')
+#req.add_header('Authorization', 'Basic bGVhcm5pbmc6bGVhcm5pbmc==')
+#req.add_header('Accept', 'application/json')
+#response = urlopen(req)
+
+#responseString = response.read().decode("utf-8")
+
+#jsonObject = json.loads(responseObject)
+
+#print(jsonObject["displayName"])
+
+#floors = jsonObject["Building"]["Floor"]
+#for floor in floors:
+#	print("Floor Number: " + str(floor["floorNumber"]))
+#	aps = floor["AccessPoint"]
+#	for ap in aps:
+#		print(" " + ap["name"] + "/" + ap["ipAddress"] + "/")
+
+
+#response.close()
+
+print("<<<end>>>")
